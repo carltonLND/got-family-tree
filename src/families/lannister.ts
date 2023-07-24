6b14e1a8c2be842206f418106d42fd7f09a259ee
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 10:18:48 +0100

    Added lannister family tree

--- src/families/lannister.ts
+++ src/families/lannister.ts
@@ -1,6 +1,27 @@
 import { Person } from "../personTypes";
 
 export function createLannisterTree(): Person {
-  //https://en.wikipedia.org/wiki/Template:Family_tree_of_Tytos_Lannister
-  return { name: "LANNISTER: YOU NEED TO IMPLEMENT THIS", children: [] };
+  const cerseiKids: Person[] = [
+    { name: "Joffrey", children: [] },
+    { name: "Myrcella", children: [] },
+    { name: "Tommen", children: [] },
+  ];
+
+  const tywinKids: Person[] = [
+    { name: "Cersei", children: cerseiKids },
+    { name: "Ser Jaime", children: [] },
+    { name: "Tyrion", children: [] },
+  ];
+
+  const tytosKids: Person[] = [
+    { name: "Tywin", children: tywinKids },
+    { name: "Genna", children: [] },
+    { name: "Kevan", children: [] },
+    { name: "Tygett", children: [] },
+    { name: "Gerion", children: [] },
+  ];
+
+  const topPerson: Person = { name: "Tytos", children: tytosKids };
+
+  return topPerson;
 }
