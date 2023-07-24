80f758c6cb12f64fd684286682718541ffdc130b
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 11:00:47 +0100

    Removed pseudocode comment

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,14 +1,5 @@
 import { Person } from "./personTypes";
 
-// function logAllPeopleInTree(rootPerson)
-//     workStack = empty stack of Person objects
-//     push rootPerson onto workStack
-
-// 		while workStack is not empty:
-//         currentPerson = pop person off workStack
-//         log the name property of currentPerson
-//         push each element of currentPerson's children onto the workStack
-
 export function logAllPeopleInTree(topPerson: Person): void {
   const workStack: Person[] = [];
   workStack.push(topPerson);
