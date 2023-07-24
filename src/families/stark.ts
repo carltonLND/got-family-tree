0f97a93a4c8e9efc4b737ae5f0c88496899c8711
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 10:18:39 +0100

    Added stark family tree

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,5 +1,23 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
-  return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  const eddardsKids: Person[] = [
+    { name: "Rickon", children: [] },
+    { name: "Bran", children: [] },
+    { name: "Arya", children: [] },
+    { name: "Sansa", children: [] },
+    { name: "Robb", children: [] },
+    { name: "Jon", children: [] },
+  ];
+
+  const rickardsKids: Person[] = [
+    { name: "Benjen", children: [] },
+    { name: "Lyanna", children: [] },
+    { name: "Eddard", children: eddardsKids },
+    { name: "Brandon", children: [] },
+  ];
+
+  const topPerson: Person = { name: "Rickard", children: rickardsKids };
+
+  return topPerson;
 }
