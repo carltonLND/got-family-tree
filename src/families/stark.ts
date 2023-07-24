fe5b16510472b62e17b06fb954e56778322baf85
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 09:56:13 +0100

    formatted

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,5 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
-
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
 }
