2739b6f2c35799d1ccae22159dd9f4fba111b4e0
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-26 10:14:14 +0100

    Added logAllPeopleInTreeWithQueue function body, added inTree boolean flag function

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,16 +1,47 @@
 import { Person } from "./personTypes";
 
-export function logAllPeopleInTree(topPerson: Person): void {
+export function logAllPeopleInTree(topPerson: Person): string[] {
+  const nameArray: string[] = [];
+
   const workStack: Person[] = [];
   workStack.push(topPerson);
 
   while (workStack.length !== 0) {
     const currentPerson = workStack.pop() as Person;
-    console.log(currentPerson.name);
+    nameArray.push(currentPerson.name);
     workStack.push(...currentPerson.children);
   }
+
+  return nameArray;
 }
 
-export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
-  console.log("TODO!  First person is " + topPerson.name);
+export function logAllPeopleInTreeWithQueue(topPerson: Person): string[] {
+  const nameArray: string[] = [];
+
+  const workQueue: Person[] = [];
+  workQueue.push(topPerson);
+
+  while (workQueue.length !== 0) {
+    const currentPerson = workQueue.shift() as Person;
+    nameArray.push(currentPerson.name);
+    workQueue.push(...currentPerson.children);
+  }
+
+  return nameArray;
+}
+
+export function inTree(targetName: string, topOfTree: Person): boolean {
+  const workStack: Person[] = [];
+  workStack.push(topOfTree);
+
+  while (workStack.length !== 0) {
+    const currentPerson = workStack.pop() as Person;
+    if (currentPerson.name === targetName) {
+      return true;
+    }
+
+    workStack.push(...currentPerson.children);
+  }
+
+  return false;
 }
