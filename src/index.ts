bb3eb0c883f578451f33ea5a4b7ed3c747f77350
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-26 10:14:18 +0100

    stuf

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,36 @@ import { createBritishSuccessionTree } from "./families/britain";
 import { createLannisterTree } from "./families/lannister";
 import { createStarkTree } from "./families/stark";
 // import { logAllPeopleInTree, logAllPeopleInTreeWithQueue } from "./solutions/solution";
-import { logAllPeopleInTree, logAllPeopleInTreeWithQueue } from "./treeLogging";
+import {
+  logAllPeopleInTree,
+  logAllPeopleInTreeWithQueue,
+  inTree,
+} from "./treeLogging";
 import { printBanner } from "./utils/bannerUtils";
 import { printPersonTreeAsAscii } from "./utils/toAsciiTree";
 
 async function mainProgram() {
-  printBanner("BARATHEON");
-  printPersonTreeAsAscii(createBaratheonTree());
-
-  printBanner("STARK");
-  printPersonTreeAsAscii(createStarkTree());
+  // printBanner("BARATHEON");
+  // printPersonTreeAsAscii(createBaratheonTree());
+  //
+  // printBanner("STARK");
+  // printPersonTreeAsAscii(createStarkTree());
 
   // Uncomment to test your work..
 
-  printBanner("LANNISTER");
-  printPersonTreeAsAscii(createLannisterTree());
+  // printBanner("LANNISTER");
+  // printPersonTreeAsAscii(createLannisterTree());
+  //
+  // printBanner("All people in STARK");
+  // logAllPeopleInTree(createStarkTree());
 
-  printBanner("All people in STARK");
-  logAllPeopleInTree(createStarkTree());
+  printBanner("WITH STACK - british royals");
+  logAllPeopleInTree(createBritishSuccessionTree());
 
-  // printBanner("WITH STACK - british royals")
-  // logAllPeopleInTree(createBritishSuccessionTree());
-  // printBanner("WITH QUEUE - british royals")
-  // logAllPeopleInTreeWithQueue(createBritishSuccessionTree());
+  printBanner("WITH QUEUE - british royals");
+  logAllPeopleInTreeWithQueue(createBritishSuccessionTree());
+
+  // inTree("Eddard", createStarkTree());
 
   // You'll need to add more test code for later exercises, too.
 }
